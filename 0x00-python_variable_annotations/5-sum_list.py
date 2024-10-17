6a65f8a1306c3469e1e8734887dbee7899c206a3
Valeryio <tommagoma23@gmail.com>
2024-10-17 12:05:56 +0000

    commit the task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
+"""THis module contain a function typed with complex structure
+like list
+"""
 
+from functools import reduce
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """It sum the different element of a list
+    param: input_list
+    return: a float
+    """
+    result: float = reduce(lambda x, y: x + y, input_list)
+    return result
