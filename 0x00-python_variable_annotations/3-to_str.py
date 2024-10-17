a7b3c39b833ea58bf4d865db3cd354cc40fed284
Valeryio <tommagoma23@gmail.com>
2024-10-17 11:45:04 +0000

    commit the task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""This module contains a function that is used to convert to str
+"""
+
+
+def to_str(n: float) -> str:
+    """It convert a float to str
+    param: n
+    return: a string
+    """
+    return str(n)
