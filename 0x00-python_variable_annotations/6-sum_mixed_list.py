60632c3539ef62655f13faed8785be9fb7e07845
Valeryio <tommagoma23@gmail.com>
2024-10-17 12:10:27 +0000

    commit the task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+THis module contains a function that sums a complex list
+"""
+
+from functools import reduce
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+    """It computes the calculation of different number types
+    param: mxd_lst
+    return: a float
+    """
+    result: float = reduce(lambda x, y: x + y, mxd_lst)
+    return result
