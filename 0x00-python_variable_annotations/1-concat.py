f227b712f43f14e6fc1f33f4946fb414659a562c
Valeryio <tommagoma23@gmail.com>
2024-10-17 11:37:16 +0000

    commit the task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""THIs modeule contains a function to concat string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function concat strings
+    param: str1
+    param: str2
+    return: A string
+    """
+    return str1 + str2
