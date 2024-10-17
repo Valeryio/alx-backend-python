b06c998c185315187e9ce58ed0965aa2e231a589
Valeryio <tommagoma23@gmail.com>
2024-10-17 11:33:31 +0000

    commit the task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+""" THis module is about a typed  add function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """THis function makes a float addtition
+    param: a
+    param: b
+    return: A bloat
+    """
+    return a + b
