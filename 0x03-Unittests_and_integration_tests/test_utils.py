28a26ed581014f131c9c9855c84d7f464956280e
Valeryio <tommagoma23@gmail.com>
2024-11-04 10:17:05 +0000

    first draft of the task 1

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""This module contains all the unittests used for
+the utils.py module
+"""
+
+import unittest
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """This class is the test class of the utils
+    module
+    """
+
+    def test_access_nested_map(self):
+        pass
