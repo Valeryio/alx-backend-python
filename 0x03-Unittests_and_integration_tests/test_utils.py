e6d23324fff8c2229cf1f3d35aeaa9e670a885b7
Valeryio <tommagoma23@gmail.com>
2024-11-04 12:03:37 +0000

    commit the task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,8 @@ the utils.py module
 
 import utils
 import unittest
+import requests
+from unittest.mock import patch
 from parameterized import parameterized
 
 
@@ -25,3 +27,37 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         result = utils.access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ({}, ["a"], KeyError),
+        ({"a": 1}, ["a", "b"], KeyError)
+        ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """This method tests the exceptions of the access_nested_map_exception
+        """
+        if expected is not None:
+            with self.assertRaises(expected):
+                result = utils.access_nested_map(nested_map, path)
+
+
+
+class TestGetJson(unittest.TestCase):
+    """This class is the test class of the utils
+    class"""
+
+    @patch("requests.get")
+    def test_get_json(self, mock_get):
+        """Patch function to test"""
+        mock_get.side_effect = [
+                unittest.mock.Mock(json=lambda: {"payload": True}),
+                unittest.mock.Mock(json=lambda: {"payload": False})
+                                   ]
+
+        result = utils.get_json("http://example.com")
+        self.assertEqual(result, {'payload': True})
+
+        result = utils.get_json("http://holberton.io")
+        self.assertEqual(result, {'payload': False})
+
+        mock_get.assert_any_call("http://example.com")
+        mock_get.assert_any_call("http://holberton.io")
